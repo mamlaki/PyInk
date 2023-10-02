ddd18188a3897663cbfe29d2e4b596040fb6a9ea
mamlaki <melekredwan@icloud.com>
2023-10-02 03:41:01 -0600

    Added menubar object. No functionality at the moment.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,28 @@
 import tkinter as tk
+from tkinter import Menu
 
 class NoteApp:
   def __init__(self, root):
     self.root = root
     root.title('PyInk')
+    
+    self.menu_bar = Menu(root)
+    root.config(menu=self.menu_bar)
+
+    self.file_menu = Menu(self.menu_bar, tearoff=0)
+    self.menu_bar.add_cascade(label='File', menu=self.file_menu)
+    self.file_menu.add_command(label='New')
+    self.file_menu.add_command(label='Open')
+    self.file_menu.add_command(label='Save')
+    self.file_menu.add_separator()
+    self.file_menu.add_command(label='Exit')
+
+    self.edit_menu = Menu(self.menu_bar, tearoff=0)
+    self.menu_bar.add_cascade(label='Edit', menu=self.edit_menu)
+    self.edit_menu.add_command(label='Cut')
+    self.edit_menu.add_command(label='Copy')
+    self.edit_menu.add_command(label='Paste')
+
     self.text_widget = tk.Text(root, wrap='word')
     self.text_widget.pack(expand=1, fill='both')
 
