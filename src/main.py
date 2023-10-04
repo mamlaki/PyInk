d602eb213eee2af39b007cc19456b944f13b372a
mamlaki <melekredwan@icloud.com>
2023-10-03 18:44:23 -0600

    Added new tab hotkey functionality.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk, Menu, messagebox
+import platform
 
 class NoteApp:
   def __init__(self, root):
@@ -9,9 +10,14 @@ class NoteApp:
     self.menu_bar = Menu(root)
     root.config(menu=self.menu_bar)
 
+    if platform.system() == 'Darwin':
+      hotkey = '⌘ T'
+    else:
+      hotkey = '^ T'
+
     self.file_menu = Menu(self.menu_bar, tearoff=0)
     self.menu_bar.add_cascade(label='File', menu=self.file_menu)
-    self.file_menu.add_command(label='New Tab', command=self.new_tab)
+    self.file_menu.add_command(label=f'New Tab ({hotkey})', command=self.new_tab)
     self.file_menu.add_command(label='New Window')
     self.file_menu.add_command(label='Open')
     self.file_menu.add_command(label='Save')
@@ -29,13 +35,16 @@ class NoteApp:
 
     self.new_tab()
 
+    root.bind('<Command-t>', self.new_tab)
+    root.bind('<Control-t>', self.new_tab)
+
   def exit_app(self):
     user_response = messagebox.askokcancel('Exit', 'Are you sure you want to exit?')
     if user_response:
       self.root.destroy()
     self.root.destroy()
 
-  def new_tab(self):
+  def new_tab(self, event=None):
     tab = tk.Frame(self.notebook)
     self.notebook.add(tab, text='Untitled')
     text_widget = tk.Text(tab, wrap='word')
